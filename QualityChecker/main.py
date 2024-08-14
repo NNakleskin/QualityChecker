1a4506e948dc47d48b469ecb8fccac9a030fd3cc
Nikita Nakleskin <nakleskin.nv@gmail.com>
2024-08-14 23:49:32 +0500

    Created interface. Added GP analyze support

--- QualityChecker/main.py
+++ QualityChecker/main.py
@@ -5,18 +5,13 @@ import logging
 import os
 import time
 
-from conf import vertica_conn_dict
+from conf import vertica_conn_dict, greenplum_conn_dict
 from utils.databaseTools import run_sql
 from utils.utils import read_file_content
 from DataQuality import DataQuality
 
 
-#TODO сделать консольный интерфейс
-ENV = 'DEV'
-connection = vertica_conn_dict[ENV]
-
 # Ниже перечислить номера проверок
-dialect = 'Vertica'
 # checks = [10]
 # 1. Дубли по ключам
 # 2. Полностью пустые столбцы (NULL или '')
@@ -32,8 +27,6 @@ dialect = 'Vertica'
 # 12. Кол-во уникальных ключей без tech_load_ts
 # 13. Дубли по ключу в stg
 # 14. Какая макс. tech_load_ts в ODS в STG
-
-
 # -------------------------------------------------------------
 
 
@@ -59,9 +52,13 @@ def main():
     print(obj_list)
     for obj in obj_list:
         table_obj = DataQuality(obj)
+        table_obj.checks = checks_list
         print(f'Начало проверки таблицы  {table_obj.schema}.{table_obj.table}  select analyze_statistics(\'{table_obj.schema}.{table_obj.table}\')')
         print(time.strftime("%Y-%m-%d %H:%M"))
-        run_sql(dialect, f'select analyze_statistics(\'{table_obj.schema}.{table_obj.table}\')', connection)
+        if dialect == 'Vertica':
+            run_sql(dialect, f'select analyze_statistics(\'{table_obj.schema}.{table_obj.table}\')', connection)
+        elif dialect == 'Greenplum':
+            run_sql(dialect, f'ANALYZE {table_obj.schema}.{table_obj.table};', connection)
         if bool(run_sql(dialect, f'select 1 from {table_obj.schema}.{table_obj.table} limit 1', connection)):
             table_obj.execute_checks(dialect, path, connection)
             table_obj.create_xlsx(path, report_name, b)
@@ -77,5 +74,58 @@ def main():
 
 
 if __name__ == '__main__':
-    print('Погнали')
+    global ENV
+    global connection
+    global dialect
+    global checks_list
+    ENV = 'DEV'
+    connection = vertica_conn_dict[ENV]
+    dialect = ''
+    checks_list = []
+    print("Какое окружение?\n",
+          "[1] DEV\n",
+          "[2] TEST\n",
+          "[3] PROD\n",)
+    env_no = int(input())
+    if env_no == 1:
+        ENV = 'DEV'
+    elif env_no == 2:
+        ENV = 'TEST'
+    elif env_no == 3:
+        ENV = 'PROD'
+    while(True):
+        print("Какой диалект нужен?\n",
+            "[1] - Vertica\n",
+            "[2] - Greenplum\n")
+        response = int(input())
+        if response == 1:
+            dialect = 'Vertica'
+            connection = vertica_conn_dict[ENV]
+            break
+        elif response == 2:
+            dialect = 'Greenplum'
+            connection = greenplum_conn_dict[ENV]
+            break
+        else:
+            print("Error: Unexpected value\n")
+    print("Перечислите через запятую номера проверок\n",
+          "0. Все\n"
+          "1. Дубли по ключам\n",
+          "2. Полностью пустые столбцы (NULL или '')\n",
+          "3. Текстовые поля длина которых достигла максимума\n",
+          "4. Наличие кривых символов (не utf-8)\n",
+          "5. Какая макс. tech_load_ts в ODS\n",
+          "6. Проверка корректности инкремента (тестируется)\n",
+          "7. Статистика самых часто встречающихся значений в поле и их доля от всех.\n",
+          "8. Статистика длин текстовых полей. varchar самого большого значения и максимальный.\n",
+          "9. Сегментация\n",
+          "10. Количество строк в STG.\n",
+          "11. Количество строк в ODS.\n",
+          "12. Кол-во уникальных ключей без tech_load_ts\n",
+          "13. Дубли по ключу в stg\n",
+          "14. Какая макс. tech_load_ts в ODS в STG\n")
+    checks_list = list(map(int, input().split(',')))
+    if 0 in checks_list:
+        checks_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+    print(ENV, dialect, checks_list)
     main()
